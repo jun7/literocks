05fb1b2ff135f5db142504a6ec07250f9a713416
jun7 <jun7@hush.com>
2022-03-13 01:38:16 +0900

    Fix wrong menu item name

--- src/menu.c
+++ src/menu.c
@@ -788,7 +788,7 @@ gboolean ensure_filer_menu(void)
 	adi(N_("About "APPNAME"..."), menu_rox_help,  HELP_ABOUT);
 	ads(N_("Show Help Files"   ), menu_rox_help,  HELP_DIR, GTK_STOCK_HELP);
 		sta(GDK_KEY_F1, 0);
-	adi(N_("About "APPNAME"..."), menu_rox_help,  HELP_MANUAL);
+	adi(N_("Manual"), menu_rox_help,  HELP_MANUAL);
 
 	start_menu(NULL, filer_menu);
 	adi(N_("Customise Dir Menu..."), customise_directory_menu, 0);
